2ee1883cbc1e5be103add4358b72953183f818c3
Tharindu Kavishna <tharinduk@suntechit.lk>
2022-11-12 10:40:17 +0530

    simple UI

---
+++ tech-iskole/exercise2/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import SimpleCard from './components/SimpleCard';
+
+function App() {
+  return <SimpleCard />
+}
+
+export default App;
---
+++ tech-iskole/exercise2/src/components/SimpleCard.js
@@ -0,0 +1,16 @@
+import "./SimpleCard.css";
+
+function SimpleCard() {
+  return (
+    <div className="card-container">
+      <h2>Introduction</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla malesuada
+        sit amet nisi at volutpat
+      </p>
+      <button>View More</button>
+    </div>
+  );
+}
+
+export default SimpleCard;
